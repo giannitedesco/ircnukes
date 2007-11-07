261ad47d5fb93c5f12b8fd0251abd973a994ce53
Gianni Tedesco <gianni@scaramanga.co.uk>
2007-11-07 22:00:59 +0000

    Cosmetic enhancements

--- nukes/player.py
+++ nukes/player.py
@@ -23,6 +23,27 @@ class player:
 	def __repr__(self):
 		return "player('%s')"%self.name
 
+	def __card_by_idx(self, idx):
+		"Return a card from the hand by index"
+
+		if idx < 0 or idx >= len(self.hand):
+			raise IllegalMoveError(self.game, self,
+						"Bad Card Index: %d"%idx)
+		assert len(self.card_stack) <= CARD_STACK_LEN
+		return self.hand.pop(idx)
+	
+	def __card_by_name(self, name):
+		"Return a card from the hand by name"
+
+		strl = map(lambda x:x.__str__().lower(), self.hand)
+		repl = map(lambda x:x.__repr__().lower(), self.hand)
+		for i in range(0, len(self.hand)):
+			if repl[i] != name.lower() and strl[i] != name.lower():
+				continue
+			return self.hand.pop(i)
+		raise IllegalMoveError(self.game, self,
+					"Card %s not found"%name)
+
 	def cards_to_hand(self):
 		"Move all cards back in to the hand"
 
@@ -33,15 +54,17 @@ class player:
 			self.weapon = None
 
 	def flip_card(self, tgt):
+		"Flip card to take a turn in the game"
+
+		if self.state != PLAYER_STATE_ALIVE:
+			raise IllegalMoveError(self.game, self,
+						"You're not alive!")
 		if self.game.cur != self:
 			raise IllegalMoveError(self.game, self,
 						"Not your turn")
 		if len(self.card_stack) != CARD_STACK_LEN:
 			raise IllegalMoveError(self.game, self,
 						"Cards not queued")
-		if self.population == 0:
-			raise IllegalMoveError(self.game, self,
-						"It's final retaliation time!")
 		c = self.card_stack.pop(0)
 		try:
 			c.dequeue(self.game, self, tgt)
@@ -50,6 +73,8 @@ class player:
 			raise
 
 	def use_card(self, arg, tgt):
+		"Use card for final retaliation"
+
 		if self.game.cur != self:
 			raise IllegalMoveError(self.game, self,
 						"Not your turn")
@@ -67,40 +92,6 @@ class player:
 		except:
 			self.hand.append(c)
 			raise
-
-	def pwn(self, pwnage):
-		"Decrement population"
-
-		i = min(self.population, pwnage)
-		self.population = self.population - i
-		if self.population == 0:
-			self.game.kill_player(self)
-
-	def transfer_population(self, converts, tgt):
-		"Transfer population to another player"
-
-		i = min(self.population, converts)
-		self.population = self.population - i
-		tgt.population = tgt.population + i
-		if self.population == 0:
-			self.game.kill_player(self)
-
-	def __card_by_idx(self, idx):
-		if idx < 0 or idx >= len(self.hand):
-			raise IllegalMoveError(self.game, self,
-						"Bad Card Index: %d"%idx)
-		assert len(self.card_stack) <= CARD_STACK_LEN
-		return self.hand.pop(idx)
-	
-	def __card_by_name(self, name):
-		strl = map(lambda x:x.__str__().lower(), self.hand)
-		repl = map(lambda x:x.__repr__().lower(), self.hand)
-		for i in range(0, len(self.hand)):
-			if repl[i] != name.lower() and strl[i] != name.lower():
-				continue
-			return self.hand.pop(i)
-		raise IllegalMoveError(self.game, self,
-					"Card %s not found"%name)
 
 	def queue_card(self, arg):
 		"Push a card in to the queue"
@@ -121,3 +112,20 @@ class player:
 			self.game.state() == GAME_STATE_INIT:
 			self.game.game_msg("%s is ready"%self.name)
 		return c
+
+	def pwn(self, pwnage):
+		"Decrement population"
+
+		i = min(self.population, pwnage)
+		self.population = self.population - i
+		if self.population == 0:
+			self.game.kill_player(self)
+
+	def transfer_population(self, converts, tgt):
+		"Transfer population to another player"
+
+		i = min(self.population, converts)
+		self.population = self.population - i
+		tgt.population = tgt.population + i
+		if self.population == 0:
+			self.game.kill_player(self)
