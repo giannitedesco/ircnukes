bb02a965787a11f52c087030672ba21ba79344e5
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-10-01 19:59:02 +0100

    Revert "Save dirty state between save games"
    This reverts commit 583377fde1ea78c51f73ee84165acbb0b403311d.

--- ircnukes.py
+++ ircnukes.py
@@ -10,7 +10,6 @@ class ircnukes(nukes.game):
 		self.__pcmd = None
 		self.__conn = None
 		self.__chan = None
-		sekf.__dirty = self.dirty
 		self.dirty = False
 
 	def save_done(self, conn, chan):
@@ -31,7 +30,7 @@ class ircnukes(nukes.game):
 				"push" : self.__push_card}
 		self.__conn = conn
 		self.__chan = chan
-		self.dirty = self.__dirty
+		self.dirty = False
 
 	def __init__(self, conn, chan, deck=None):
 		self.save_done(conn, chan)
